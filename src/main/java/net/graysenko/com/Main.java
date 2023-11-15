7fd537fb272b16d6c75af2cf8fb1c56d0a150aed
user27012 <kolabelozorov@gmail.com>
2023-11-16 00:29:12 +0100

    AnnotationNew

---
+++ src/main/java/net/graysenko/com/Main.java
@@ -0,0 +1,35 @@
+package net.graysenko.com;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Method;
+
+class Calculate {
+
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface calc {
+        int first() default 4;
+        int second() default 12;
+    }
+}
+
+public class Main {
+
+    @Calculate.calc(first = 10, second = 21)
+    public static void main(String[] args) {
+        Calculate.calc annotation = null;
+        try {
+            Method mainMethod = Main.class.getMethod("main", String[].class);
+            annotation = mainMethod.getAnnotation(Calculate.calc.class);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+
+        if (annotation != null) {
+            int first = annotation.first();
+            int second = annotation.second();
+            int sum = first + second;
+            System.out.println("Sum: " + sum);
+        }
+    }
+}
